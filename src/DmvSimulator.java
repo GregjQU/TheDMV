a34ddbe86c224da5d661cd3620c699376f8c4b06
GregjQU <gjeanbaptiste@quinnipiac.edu>
2022-05-15 15:37:59 -0400

    added 1st portion

--- src/DmvSimulator.java
+++ src/DmvSimulator.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class DmvSimulator {
 
@@ -5,10 +6,38 @@ public class DmvSimulator {
 		// TODO Auto-generated method stub
 
 		
-		System.out.println("welcome to the DMV. How may we help you");
+		System.out.println("welcome to the DMV. How may we help you?");
 		
-		System.out.println("hello");
+		
 		///xyz
+		
+		int[] num = new int[500];		
+        int userPlace = new Random().nextInt(num.length);
+        System.out.println("Your number is " + userPlace + "! Please wait to be called.");
+
+        countUp(userPlace);
+        countDown(userPlace);
+      
+        if( Math.random() <= 0.75 ) {
+            System.out.println("You  have your proper forms. Please proceed to the next line.");
+          }
+        else
+        {
+        System.out.println("You don't have the proper papers. Please come back later");
+
+        }
+    }
+
+	private static void countDown(int userPlace) {
+		// TODO Auto-generated method stub
+		
 	}
 
+	private static void countUp(int userPlace) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
 }
+
